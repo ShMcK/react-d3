51ead4378072c7f4edd2d53ab05fb6323d26656f
ShMcK <shawn.j.mckay@gmail.com>
2016-11-21 19:51:38 -0800

    setup react-storybook

---
+++ .storybook/addons.js
@@ -0,0 +1,11 @@
+import '@kadira/storybook/addons'
+
+// adjustable knobs
+import '@kadira/storybook-addon-knobs/register'
+
+// readme
+import 'storybook-readme/register'
+
+// comments & cloud-based storybook links
+import '@kadira/storybook-database-cloud/register';
+import '@kadira/storybook-addon-comments/register';
